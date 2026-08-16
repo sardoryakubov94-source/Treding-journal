0e809a3879a7145ff7ed1709129384f923271bb0
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-08-16 22:48:53 +0500

    Update cache version from v17 to v18

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Diqqat: Firebase/Firestore ma'lumotlari baribir internet talab qiladi,
 // bu SW faqat ilova qobig'ini (HTML/CSS/JS/rasm) tezroq yuklaydi.
 
-const CACHE_VERSION = 'tj-cache-v17';
+const CACHE_VERSION = 'tj-cache-v18';
 const APP_SHELL = [
   './',
   './index.html',
