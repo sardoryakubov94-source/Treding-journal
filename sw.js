d572cb054e00e79a9628349db6601464de4d233a
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-08-16 20:49:56 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Diqqat: Firebase/Firestore ma'lumotlari baribir internet talab qiladi,
 // bu SW faqat ilova qobig'ini (HTML/CSS/JS/rasm) tezroq yuklaydi.
 
-const CACHE_VERSION = 'tj-cache-v11';
+const CACHE_VERSION = 'tj-cache-v12';
 const APP_SHELL = [
   './',
   './index.html',
