5841f626a7b92bb1caa24d8ca1fff948d1214625
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-08-16 14:18:59 +0500

    Update cache version to tj-cache-v7

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Diqqat: Firebase/Firestore ma'lumotlari baribir internet talab qiladi,
 // bu SW faqat ilova qobig'ini (HTML/CSS/JS/rasm) tezroq yuklaydi.
 
-const CACHE_VERSION = 'tj-cache-v6';
+const CACHE_VERSION = 'tj-cache-v7';
 const APP_SHELL = [
   './',
   './index.html',
