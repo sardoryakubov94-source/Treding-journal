57cf7aa314fe63000d29b69dd182ffa31222b881
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-08-17 09:19:52 +0500

    Update cache version from v22 to v23

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Diqqat: Firebase/Firestore ma'lumotlari baribir internet talab qiladi,
 // bu SW faqat ilova qobig'ini (HTML/CSS/JS/rasm) tezroq yuklaydi.
 
-const CACHE_VERSION = 'tj-cache-v22';
+const CACHE_VERSION = 'tj-cache-v23';
 const APP_SHELL = [
   './',
   './index.html',
