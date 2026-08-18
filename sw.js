cc3ff5f847d40eca1405590f32e996c55bbb0760
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-08-18 12:54:27 +0500

    Update cache version from v31 to v32

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Diqqat: Firebase/Firestore ma'lumotlari baribir internet talab qiladi,
 // bu SW faqat ilova qobig'ini (HTML/CSS/JS/rasm) tezroq yuklaydi.
 
-const CACHE_VERSION = 'tj-cache-v31';
+const CACHE_VERSION = 'tj-cache-v32';
 const APP_SHELL = [
   './',
   './index.html',
