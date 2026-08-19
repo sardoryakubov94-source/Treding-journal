94c0d5ae8de9835b1afcba3e5c1101a1a551c126
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-08-19 08:47:08 +0500

    Update cache version to 'tj-cache-v46'

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Diqqat: Firebase/Firestore ma'lumotlari baribir internet talab qiladi,
 // bu SW faqat ilova qobig'ini (HTML/CSS/JS/rasm) tezroq yuklaydi.
 
-const CACHE_VERSION = 'tj-cache-v45';
+const CACHE_VERSION = 'tj-cache-v46';
 const APP_SHELL = [
   './',
   './index.html',
