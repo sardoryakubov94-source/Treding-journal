ad4696902f1ea78cf12fd8937dfb2f0707322cd5
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-08-19 18:36:09 +0500

    Increment cache version from v52 to v53
    Updated cache version to v53.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Diqqat: Firebase/Firestore ma'lumotlari baribir internet talab qiladi,
 // bu SW faqat ilova qobig'ini (HTML/CSS/JS/rasm) tezroq yuklaydi.
 
-const CACHE_VERSION = 'tj-cache-v52';
+const CACHE_VERSION = 'tj-cache-v53';
 const APP_SHELL = [
   './',
   './index.html',
