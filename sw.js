204ff21dd4156232bb00617610a9ac9c3a8df427
sardoryakubov94-source <sardoryakubov94@gmail.com>
2026-08-31 23:50:33 +0500

    Update cache version to v62 in service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Diqqat: Firebase/Firestore ma'lumotlari baribir internet talab qiladi,
 // bu SW faqat ilova qobig'ini (HTML/CSS/JS/rasm) tezroq yuklaydi.
 
-const CACHE_VERSION = 'tj-cache-v61';
+const CACHE_VERSION = 'tj-cache-v62';
 const APP_SHELL = [
   './',
   './index.html',
